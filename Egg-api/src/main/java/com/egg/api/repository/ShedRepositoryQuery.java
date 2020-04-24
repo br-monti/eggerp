d2791046a06e88f64b53c8c3b5c880e9c82326a1
br-monti <bruno.smsouza@gmail.com>
2020-04-24 15:49:44 -0300

    Criando as classes para filtro de Shed

---
+++ Egg-api/src/main/java/com/egg/api/repository/ShedRepositoryQuery.java
@@ -0,0 +1,13 @@
+package com.egg.api.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.egg.api.model.Shed;
+import com.egg.api.repository.filter.ShedFilter;
+
+public interface ShedRepositoryQuery  {
+	
+	public Page<Shed> findByFilter(ShedFilter shedFilter, Pageable pageable);
+
+}
---
+++ Egg-api/src/main/java/com/egg/api/repository/filter/ShedFilter.java
@@ -0,0 +1,18 @@
+package com.egg.api.repository.filter;
+
+public class ShedFilter {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	
+
+}
