ba090b2b90646d8a95499793e2f931bec2906f61
br-monti <bruno.smsouza@gmail.com>
2020-06-05 16:00:54 -0300

    Gerando os metalmodels

---
+++ Egg-api/src/main/java/com/egg/api/model/Packing_.java
@@ -0,0 +1,26 @@
+package com.egg.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Packing.class)
+public abstract class Packing_ {
+
+	public static volatile SingularAttribute<Packing, String> name;
+	public static volatile SingularAttribute<Packing, Integer> quantityByBox;
+	public static volatile SingularAttribute<Packing, Long> id;
+	public static volatile SingularAttribute<Packing, Integer> packingByBox;
+	public static volatile SingularAttribute<Packing, String> packingType;
+	public static volatile SingularAttribute<Packing, Integer> quantityByPacking;
+
+	public static final String NAME = "name";
+	public static final String QUANTITY_BY_BOX = "quantityByBox";
+	public static final String ID = "id";
+	public static final String PACKING_BY_BOX = "packingByBox";
+	public static final String PACKING_TYPE = "packingType";
+	public static final String QUANTITY_BY_PACKING = "quantityByPacking";
+
+}
+
---
+++ Egg-api/src/main/java/com/egg/api/model/Product_.java
@@ -0,0 +1,22 @@
+package com.egg.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Product.class)
+public abstract class Product_ {
+
+	public static volatile SingularAttribute<Product, String> nick;
+	public static volatile SingularAttribute<Product, EggType> eggType;
+	public static volatile SingularAttribute<Product, Long> id;
+	public static volatile SingularAttribute<Product, Packing> packing;
+
+	public static final String NICK = "nick";
+	public static final String EGG_TYPE = "eggType";
+	public static final String ID = "id";
+	public static final String PACKING = "packing";
+
+}
+
