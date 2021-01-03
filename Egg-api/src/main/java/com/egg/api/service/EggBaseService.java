4a567b49e8029a8253498e654f9a089364427f03
Bruno <bruno.smsouza@gmail.com>
2021-01-03 20:10:40 -0200

    Criando classifications a partir de EggBase

--- Egg-api/src/main/java/com/egg/api/service/EggBaseService.java
+++ Egg-api/src/main/java/com/egg/api/service/EggBaseService.java
@@ -1,18 +1,31 @@
 package com.egg.api.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.egg.api.model.Classification;
 import com.egg.api.model.EggBase;
+import com.egg.api.model.EggType;
 import com.egg.api.repository.EggBaseRepository;
+import com.egg.api.repository.EggTypeRepository;
 
 @Service
 public class EggBaseService {
 	
 	@Autowired
 	private EggBaseRepository eggBaseRepository;
+	
+	@Autowired
+	private EggTypeRepository eggTypeRepository;
+	
+	
+	
+
 
 	public EggBase update(Long id, EggBase eggBase) {
 		EggBase eggBaseSaved = findEggBaseById(id);
@@ -36,19 +49,21 @@ public class EggBaseService {
 
 
 	public EggBase save(EggBase eggBase) {
-		eggBase.getClassifications().forEach(c -> c.setEggBase(eggBase));
+		List <EggType> eggTypeList = eggTypeRepository.findAll();
+		
+		List <Classification> classificationList = new ArrayList<>();
+
+		Classification classification = null;
+		for (EggType eggType : eggTypeList) {
+			classification = new Classification();
+			classification.setEggBase(eggBase);	
+			classification.setEggType(eggType);
+			classificationList.add(classification);
+		}		
+		
+		eggBase.setClassifications(classificationList);		
+		
 		return eggBaseRepository.save(eggBase);
 	}
-	
-	
-//	List <EggType> eggTypeList = eggTypeRepository.findAll();
-//	int index = 0;
-//	
-//	for (Classification classification : eggBase.getClassifications()) {
-//		classification.setEggBase(eggBase);			
-//		classification.setEggType(eggTypeList.get(index));
-//		index++;
-//	}
-//	index = 0;
 
 }
